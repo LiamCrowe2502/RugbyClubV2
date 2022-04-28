12a9979033b51de35acc05e8025d6d191156cc39
liamc <20093470@mail.wit.ie>
2022-04-28 22:37:19 +0100

    Added the list menu for the interface. I also added the Scanner details to gather input from the user. This project does not get the input from a user. I also added a logger through the gradle and added the logger to each function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,83 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import mu.KotlinLogging
+import java.util.*
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+private val logger = KotlinLogging.logger {}
+val scanner = Scanner(System.`in`)
+
+fun main(args: Array<String>) {
+    runMenu()
+}
+
+fun mainMenu() : Int {
+    print(""" 
+         > ----------------------------------
+         > |        Rugby Club APP         |
+         > ----------------------------------
+         > |   1) Add a player              |
+         > |   2) List all players          |
+         > |   3) List number of players    |
+         > |   4) Search player ID          |
+         > |   5) Save player details       |
+         > |   6) Load database             |
+         > |   7) Update player details     |
+         > |   8) Delete a player           |
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
+    return scanner.nextInt()
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> addPlayer()
+            2  -> listPlayers()
+            3  -> numOfPlayers()
+            4  -> searchByID()
+            5  -> save()
+            6  -> load()
+            7  -> updatePlayer()
+            8  -> deletePlayer()
+            0  -> exit()
+            else -> System.out.println("Invalid option entered: ${option}")
+        }
+    } while (true)
+}
+
+fun addPlayer(){
+    logger.info { "addPlayer() function invoked" }
+}
+
+fun listPlayers() {
+    logger.info { "listPlayers() function invoked" }
+}
+
+fun numOfPlayers() {
+    logger.info { "numberOfPlayers() function invoked" }
+}
+
+fun searchByID() {
+    logger.info { "searchByID() function invoked" }
+}
+
+fun save() {
+    logger.info { "save() function invoked" }
+}
+
+fun load() {
+    logger.info { "load() function invoked" }
+}
+
+fun updatePlayer() {
+    logger.info { "updatePlayers() function invoked" }
+}
+
+fun deletePlayer() {
+    logger.info { "deletePlayer() function invoked" }
+}
+
+fun exit() {
+    logger.info { "exit function invoked" }
+    System.exit(0)
 }
