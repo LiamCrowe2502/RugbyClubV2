8c1940a850f2084c890c0c9b7a070fef54faad56
liamc <20093470@mail.wit.ie>
2022-04-30 00:31:42 +0100

    I have two pieces of validation. I have validation for the age which is between a range and I have now formatted the string to be in a specific way. 'yyyy-mm-dd'

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -1,13 +1,25 @@
 package model
 
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
+//create an extension function on a date class which returns a string
+private fun Date.dateToString(format: String): String {
+    //simple date formatter
+    val dateFormatter = SimpleDateFormat(format, Locale.getDefault())
+
+    //return the formatted date string
+    return dateFormatter.format(this)
+}
+
 class Player(
     var Name:String = "",
-    var DOB: String = "",
-    var ageGroup:Int,
+    var ageGroup:Int = 18,
     //var DOB: String = ""
+    var timestamp: Date = Date(),
+    var DOB: String = timestamp.dateToString("YYYY-MM-DD")
     //var DOB: LocalDate? = LocalDate.parse("yyyy-MM-dd")
 ) {
 
@@ -17,7 +29,7 @@ class Player(
 
     val playerID = count.incrementAndGet()
     override fun toString(): String {
-        return "Player(id=$playerID, name ='$Name', age ='$ageGroup' dob ='$DOB')"
+        return "Player(id=$playerID, name = '$Name', age = '$ageGroup' dob = '$DOB')"
     }
 
 }
