5496bb26eda8a786539ee806a049bf1ab4318ee8
liamc <20093470@mail.wit.ie>
2022-04-29 20:35:54 +0100

    Added the data class which will have the attributes of Name, ageGroup, and DOB.

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -1,22 +1,8 @@
 package model
 
-import java.time.LocalDate
-import java.util.concurrent.atomic.AtomicInteger
-
-class Player(
+data class Player(
     var Name:String = "",
     var ageGroup:Int = 18,
-    var DOB: String = ""
-    //var DOB: LocalDate? = LocalDate.parse("yyyy-MM-dd")
-) {
-
-    companion object {
-        private val count: AtomicInteger = AtomicInteger(100)
-    }
-
-    val playerID = count.incrementAndGet()
-    override fun toString(): String {
-        return "Player(id=$playerID, name='$Name', age='$ageGroup' dob='$DOB')"
-    }
+    var DOB: String = "") {
 
 }
